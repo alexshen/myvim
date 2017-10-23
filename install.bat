fe4abb63a7cceb6ec227c9ddc09945b1d52c46ec
Alex Shen <swlsww@hotmail.com>
2017-10-23 21:20:06 +0800

    remove code for setting up ycm-win

--- install.bat
+++ install.bat
@@ -2,9 +2,6 @@
 
 setlocal
 
-if "%1"=="" set version=win32
-if not "%1"=="" set version=%1
-
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
 if exist %HOME%\.vimrc del %HOME%\.vimrc
@@ -15,15 +12,3 @@ mklink %HOME%\.gvimrc "%~dp0\_gvimrc"
 
 del %HOME%\.vim
 mklink /D %HOME%\.vim "%~dp0\vimfiles"
-
-set ycmd=%~dp0\vimfiles\bundle\ycm-win\third_party\ycmd
-set ycm_bin_ver=%ycmd%\%version%
-
-del "%ycmd%\libclang.dll"
-mklink "%ycmd%\libclang.dll" "%ycm_bin_ver%\libclang.dll"
-
-del "%ycmd%\ycm_client_support.pyd"
-mklink "%ycmd%\ycm_client_support.pyd" "%ycm_bin_ver%\ycm_client_support.pyd"
-
-del "%ycmd%\ycm_core.pyd"
-mklink "%ycmd%\ycm_core.pyd" "%ycm_bin_ver%\ycm_core.pyd"
