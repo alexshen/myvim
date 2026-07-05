fc8411d7478860a5ec21041e4f049e653ebd2dbb
Alex Shen <swlsww@hotmail.com>
2026-07-05 20:44:50 +0800

    renamed _vimrc and _gvimrc

--- install.bat
+++ install.bat
@@ -1,14 +1,24 @@
 @echo off
-
 setlocal
 
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
+echo Installing Vim config...
+
 if exist %HOME%\.vimrc del %HOME%\.vimrc
-mklink %HOME%\.vimrc "%~dp0\_vimrc"
+mklink %HOME%\.vimrc "%~dp0.vimrc"
 
 if exist %HOME%\.gvimrc del %HOME%\.gvimrc
-mklink %HOME%\.gvimrc "%~dp0\_gvimrc"
+mklink %HOME%\.gvimrc "%~dp0.gvimrc"
 
 del %HOME%\.vim
 mklink /D %HOME%\.vim "%~dp0\vimfiles"
+
+echo.
+echo Installing Neovim config...
+
+set NVIM_DIR=%LOCALAPPDATA%\nvim
+if not exist "%NVIM_DIR%" mkdir "%NVIM_DIR%"
+
+if exist "%NVIM_DIR%\init.vim" del "%NVIM_DIR%\init.vim"
+mklink "%NVIM_DIR%\init.vim" "%~dp0init.vim"
--- install.sh
+++ install.sh
@@ -4,36 +4,30 @@ vim_dir=$(cd $(dirname "$0") && pwd)
 
 prompt_overwrite() {
     local path="$1"
-    local label="$2"
+    local name="$(basename "$path")"
     if [ -f "$path" ] || [ -L "$path" ]; then
-        echo -n "$label already exists, overwrite?(y/n) "
+        echo -n "$name already exists, overwrite?(y/n) "
         read answer
-        case $answer in
-            y|Y) rm -f "$path"
-                 ln -s "$vim_dir/$3" "$path"
-                 echo "  $path -> $vim_dir/$3"
-                 ;;
-        esac
-    else
-        ln -s "$vim_dir/$3" "$path"
-        echo "  $path -> $vim_dir/$3"
+        case $answer in y|Y) rm -f "$path" ;; *) return ;; esac
     fi
+    ln -s "$vim_dir/$2" "$path"
+    echo "  $path -> $vim_dir/$2"
 }
 
 echo "Installing Vim config..."
 echo ""
 
 # Vim: .vimrc
-prompt_overwrite ~/.vimrc ".vimrc" "_vimrc"
+prompt_overwrite ~/.vimrc ".vimrc"
 
 # Vim: .gvimrc
-prompt_overwrite ~/.gvimrc ".gvimrc" "_gvimrc"
+prompt_overwrite ~/.gvimrc ".gvimrc"
 
 # Vim: vimfiles -> .vim
 if [ -d ~/.vim ] && [ ! -L ~/.vim ]; then
     echo "  .vim is a directory, skipping (remove manually if you want to replace it)"
 else
-    prompt_overwrite ~/.vim ".vim" "vimfiles"
+    prompt_overwrite ~/.vim "vimfiles"
 fi
 
 echo ""
@@ -42,4 +36,4 @@ echo "Installing Neovim config..."
 # Neovim: init.vim
 nvim_init_dir=~/.config/nvim
 mkdir -p "$nvim_init_dir"
-prompt_overwrite "$nvim_init_dir/init.vim" "init.vim" "init.vim"
+prompt_overwrite "$nvim_init_dir/init.vim" "init.vim"
